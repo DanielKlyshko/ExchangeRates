902d7c362cdacb3691c05a65ccb58ce8a79f66ea
DanielKlyshko <danielklyshko29@yandex.com>
2024-01-16 21:07:33 +0300

    XCode project added

---
+++ ExchangeRates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExchangeRates
+//
+//  Created by Daniel Klyshko on 16.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
